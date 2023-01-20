d8280e3099596ab4d705c6e57dbc405274a956f3
tomoya-onuki <tomoya.onuki28@gmail.com>
2023-01-20 11:29:36 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -1,71 +1,52 @@
-import * as d3 from 'd3';
+import * as THREE from "three";
 
 window.addEventListener('load', () => {
     init();
 }, false);
 
-const width: number = 600;
-const height: number = 400;
-const margin = {
-    top : 50, right: 50, bottom: 100, left: 50
-};
 
-function init(): void {
-    // データの読み込み
-    d3.csv('data.csv')
-        .then((data) => {
-            draw(data);
-        })
-        .catch((error) => {
-            console.log(error)
-        });
+let cvsWidth: number = 600;
+let cvsHeight: number = 600;
+let renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({ alpha: true });
+let scene: THREE.Scene = new THREE.Scene();
+let camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(50, cvsWidth / cvsHeight, 1, 2000);
+let cube: THREE.Mesh;
+
+function init() {
+    let elem = <HTMLElement>document.querySelector('body');
+    elem.append(renderer.domElement);
+    camera.position.set(50, 50, 50);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(cvsWidth, cvsHeight);
+    camera.aspect = cvsWidth / cvsHeight;
+    camera.updateProjectionMatrix();
+
+    const light = new THREE.DirectionalLight(0xffffff);
+    light.position.set(1, 1, 1);
+    scene.add(light);
+
+    const axes = new THREE.AxesHelper(10);
+    scene.add(axes);
+
+    draw();
+    render();
 }
 
+function render() {
+    requestAnimationFrame(render);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
 
-function draw(data: d3.DSVRowArray): void {
-
-    let chartWidth: number = width - margin.left - margin.right;
-    let chartHeight: number = height - margin.top - margin.bottom;
-
-    // SVGの設定
-    const svg: any = d3.select('body')
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height);
-
-    console.log(svg);
-
-    // x axis
-    let xScale: any = d3.scaleTime()
-        .domain(<Date[]>d3.extent(data, (d: d3.DSVRowString) => new Date(String(d.Date)) ))
-        .range([0, chartWidth]);
-    svg.append('g')
-        .attr('transform', `translate(${margin.left}, ${Number(margin.top + chartHeight)})`)
-        .call(d3.axisBottom(xScale).tickFormat(<any>d3.timeFormat('%Y/%m/%d')) )
-        .selectAll('text')
-        .attr('transform', 'translate(25, 35)rotate(60)');
-
-
-    // y axis
-    let yScale: any = d3.scaleLinear()
-        .domain([ 0, Number(d3.max(data, (d: d3.DSVRowString) => +Number(d.ALL) )) ])
-        .range([chartHeight, 0]);
-    svg.append('g')
-        .attr('transform', `translate(${margin.left}, ${margin.top})`)
-        .call(d3.axisLeft(yScale));
-
-
-    // 折線
-    const line: any = d3.line()
-        .x((d: any) => xScale(new Date(String(d.Date))))
-        .y((d: any) => yScale(Number(d.ALL)));
-
-    // 描画
-    svg.append('path')
-        .datum(data)
-        .attr('transform', `translate(${margin.left}, ${margin.top})`)
-        .attr('fill', 'none')
-        .attr('stroke', 'steelblue')
-        .attr('stroke-width', 1.5)
-        .attr('d', line);
+function draw() {
+    const geometry = new THREE.BoxGeometry(10, 10, 10);
+    const material = new THREE.MeshLambertMaterial({
+        color: new THREE.Color('#4da9ff'),
+        transparent: true,
+        opacity: 0.5
+    });
+    cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
 }
